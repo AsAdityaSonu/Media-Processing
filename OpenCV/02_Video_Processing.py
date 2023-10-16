91cb8f229101acd2f24af7ab2152220d4d339819
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2023-10-16 13:14:36 +0530

    Update 02_Video_Processing.py
    comments added

--- OpenCV/02_Video_Processing.py
+++ OpenCV/02_Video_Processing.py
@@ -3,15 +3,19 @@ import cv2
 cap = cv2.VideoCapture(0)  # (Camera Number)
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read()  # return 2 values
 
     cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # (Source, conversion)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame', gray)
+    # print(frame)  #will return matrix of image captured by camera
+    # print(ret)    #will return true until camera is on
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame', frame)  # (windows name, matrix)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # if cv2.waitKey(1) & 0xFF == ord('q') checks if the key pressed is 'q'.
+
         break
 
 cap.release()
